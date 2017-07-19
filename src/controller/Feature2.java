0d0ad710e3d5c4c06b8034e1297935cf6679e82c
Hlaing Thazin Aung <cherryhtza@gmail.com>
2017-07-19 15:52:03 +0630

    I edited feature 2 also

--- src/controller/Feature2.java
+++ src/controller/Feature2.java
@@ -3,6 +3,7 @@ package controller;
 public class Feature2 {
 	public static void main(String []args){
 		System.out.println("Feature 2");
+		System.out.println("I edited feature 2 also !!!");
 	}
 
 }
